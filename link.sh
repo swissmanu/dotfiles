168e1c56ab11cd79e52911e99d8e58cb8dc44aeb
Manuel Alabor <manuel@alabor.me>
2020-01-29 13:22:28 +0100

    Add alacritty Config

--- link.sh
+++ link.sh
@@ -2,7 +2,7 @@
 
 # Install dotfiles
 # Shamelessly copied from github.com/pstadler/
-for symlink in hammerspoon tmux.conf gitconfig gitignore_global zshrc vimrc fzfcommands
+for symlink in hammerspoon tmux.conf gitconfig gitignore_global zshrc vimrc fzfcommands alacritty.yml
 do
 	rm ~/.$symlink
 	ln -s $PWD/$symlink ~/.$symlink
