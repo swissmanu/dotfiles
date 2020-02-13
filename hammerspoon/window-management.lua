9f1350edd6e5aaba49fcfe08066111c133352fc0
Manuel Alabor <manuel@alabor.me>
2020-02-13 07:32:26 +0100

    (hammerspoon) Add Screen Mngmt Spoon

--- hammerspoon/window-management.lua
+++ hammerspoon/window-management.lua
@@ -1,10 +1,17 @@
 hs.loadSpoon("MiroWindowsManager")
+hs.loadSpoon("WindowScreenLeftAndRight")
 
 hs.window.animationDuration = 0.01
 spoon.MiroWindowsManager:bindHotkeys({
-  up = {hyper, "up"},
-  right = {hyper, "right"},
-  down = {hyper, "down"},
-  left = {hyper, "left"},
-  fullscreen = {hyper, "m"}
+    up = {hyper, "up"},
+    right = {hyper, "right"},
+    down = {hyper, "down"},
+    left = {hyper, "left"},
+    fullscreen = {hyper, "m"}
 })
+
+spoon.WindowScreenLeftAndRight:bindHotkeys(
+    {
+        screen_left = {hyper, "j"},
+        screen_right = {hyper, "k"}
+    })
